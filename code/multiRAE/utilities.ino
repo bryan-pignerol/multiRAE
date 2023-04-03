f124de984a78b1647690778d1e54d5e94e4582c7
bryan-pignerol <90550463+bryan-pignerol@users.noreply.github.com>
2023-04-03 08:47:59 +0200

    Add files via upload

--- code/multiRAE/utilities.ino
+++ code/multiRAE/utilities.ino
@@ -1,12 +1,12 @@
 // DISPLAY FUNC
 void lcdReversePrint(Adafruit_PCD8544 &lcdDisplay, String &str, int col, int row) {
-  for (int i = str.length() - 1; i > -1; i--) {
-    lcdDisplay.drawChar(col - (str.length() - 1 - i) * 6, row, str[i], 255, 0, 1, 1);
+  for(int i = str.length() - 1; i > -1; i--) {
+    lcdDisplay.drawChar(col - (str.length() - 1 - i) * 6, row, str[i], 255, 0, 1, 1); 
   }
 }
 
 void lcdErase(Adafruit_PCD8544 &lcdDisplay, int col, int row, int numChar) {
-  for (int i = 0; i < numChar; i++) {
+  for(int i = 0; i < numChar; i++) {
     lcdDisplay.drawChar(col + 6 * i, row, ' ', 255, 0, 1, 1);
   }
 }
@@ -27,14 +27,14 @@ void refreshVariables() {
   lcdErase(nokiaDisplay, 6, 40, 3);
   lcdErase(nokiaDisplay, 44, 40, 4);
   nokiaDisplay.display();
-  delay(1000);  // delay à retirer à l'avenir
+  delay(1000); // delay à retirer à l'avenir
 }
 
 
 // BUZZER FUNC
 void buzz(float freq) {
-  tone(buzzer, 500, 500);
-  delay((1 / freq) * 1000);
+  tone(BUZZER, 500, 500);
+  delay((1/freq)*1000);
 }
 
 // XBEE FUNC
